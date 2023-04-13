f68ce6a597591a6e9c6130ef517d13340aed71e0
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-13 16:53:34 -0300

    Add files via upload

---
+++ Somar Vetores/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somar_Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numerosEntrada = new int[5];
+            int contador = 0;
+
+            while (contador < numerosEntrada.Length)
+            {
+                Console.WriteLine("Digite os números a serem somados:");
+                numerosEntrada[contador] = int.Parse(Console.ReadLine());
+                contador++;
+                
+                Console.Clear();
+            }
+
+            int contador2 = 0;
+            int soma = 0;
+            while (contador2 < numerosEntrada.Length) 
+            {
+                soma += numerosEntrada[contador2];
+                contador2++;
+
+                Console.Clear();
+                Console.WriteLine("A soma é: " + soma);               
+            }
+
+            int contador3 = 0;  
+            while(contador3 < numerosEntrada.Length) 
+            {
+                Console.WriteLine("Os números somados são: " + numerosEntrada[contador3]);
+                contador3++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
